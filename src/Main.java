daef82672966321bb8299048e93348e73bed7d79
MARDELBE <marianabeltran121@gmail.com>
2026-04-19 20:57:55 -0500

    Se agrego validacion de correo, validacion de numero y excepcion de formato no valido,falta agregar la busqueda y excepcion de no existe t duplicados, por ultimo se agregará JoptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
-
-
-
-
-
+import java.util.regex.Pattern;
 public class Main{
-    static void main(String[] args) {
-        try{
-            Scanner Consola = new Scanner(System.in);
-            Directorio agenda = new Directorio();
-            System.out.println("Bienvenido: Ingrese un nuevo conacto ");
-            System.out.print("Ingrese el nombre del contacto: ");
-            String name = Consola.nextLine();
-            System.out.print("Ingrese el telefono  del contacto con indicativo (+) :");
-            String telefono = Consola.nextLine();
-            validarNumero(telefono);
-            System.out.print("Ingrese el correo  del contacto: ");
-            String correo = Consola.nextLine();
-            agenda.agregarPersona(name,telefono,correo);
-            System.out.println(agenda.mostrarDirectorio());
-        }catch (MalFormatoException e){
 
-        }
+    private static final Pattern PATRON_CORREO = Pattern.compile(
+            "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+    );
+
+    public static void main(String[] args) {
+
+        do {
+            try{
+                Scanner Consola = new Scanner(System.in);
+                Directorio agenda = new Directorio();
+                System.out.println("Bienvenido: Ingrese un nuevo conacto ");
+                System.out.print("Ingrese el nombre del contacto: ");
+                String name = Consola.nextLine();
+                System.out.print("Ingrese el telefono  del contacto con indicativo (+) :");
+                String telefono = Consola.nextLine();
+                validarNumero(telefono);
+                System.out.print("Ingrese el correo  del contacto: ");
+                String correo = Consola.nextLine();
+                validarCorreo(correo);
+                agenda.agregarPersona(name,telefono,correo);
+                System.out.println(agenda.mostrarDirectorio());
+            }catch (MalFormatoException e){
+                System.out.println(e.getMessage());
+            }
+        }while(true);
+
 
 
     }
@@ -33,6 +39,9 @@ public class Main{
         }
     }
     public static void validarCorreo(String correo) throws MalFormatoException{
+        if(!PATRON_CORREO.matcher(correo).matches()){
+            throw new MalFormatoException("Este correo no tiene un formato valido");
+        }
 
     }
 
---
+++ src/ValidarCorreo.java
@@ -0,0 +1,7 @@
+import java.util.regex.Pattern;
+
+public class ValidarCorreo {
+
+
+
+}
